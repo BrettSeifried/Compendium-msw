61a11ae32752d3acc7aca7c186677cb905df6be1
Brett Seifried <seifriedbrett@gmail.com>
2022-01-25 12:39:33 -0800

    images showing up

--- src/component/card.js
+++ src/component/card.js
@@ -7,6 +7,7 @@ export default function DisplayData({ data: data }) {
       {data.map((kuroko) => (
         <p key={kuroko.mal_id}>
           <h1>{kuroko.title}</h1>
+          <img src={kuroko.images.jpg.image_url} alt="image"></img>
         </p>
       ))}
       ;
